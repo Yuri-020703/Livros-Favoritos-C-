d741eb406f60fc72d1885978dd82e52bf035f420
Richard Yuri <richardyuri930@gmail.com>
2025-03-11 20:09:05 -0300

    Add files via upload

---
+++ Livros.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Livros_Favoritos
+{
+    internal class Livros
+    {
+        public string Titulo { get; private set; }
+        public string Autor { get; private set; }
+        public int Ano { get; private set; }
+        public int Páginas { get; private set; }
+
+        public void SetTitulo(string x)
+        {
+            Titulo = x;
+        }
+
+        public void SetAutor(string x)
+        {
+            Autor = x;
+        }
+
+        public void SetAno(int x)
+        {
+            Ano = x;
+        }
+
+        public void SetPaginas(int x)
+        {
+            Páginas = x;
+        }
+
+        public override string ToString()
+        {
+            return "Título: " + Titulo + ", Autor: " + Autor + ", Ano de lançamento: " + Ano + ", Número de páginas: " + Páginas;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Livros_Favoritos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos livros favoritos você tem ? ");
+            int n = int.Parse(Console.ReadLine());
+
+            Livros[] vect =  new Livros[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                vect[i] = new Livros();
+
+                Console.WriteLine("Digite os dados do Livro " + (i + 1));
+
+                Console.Write("Título: ");
+                string titulo = Console.ReadLine();
+                vect[i].SetTitulo(titulo);
+                Console.Write("Autor: ");
+                string autor = Console.ReadLine();
+                vect[i].SetAutor(autor);
+                Console.Write("Ano de lançamento: ");
+                int ano = int.Parse(Console.ReadLine());
+                vect[i].SetAno(ano);
+                Console.Write("Número de páginas: ");
+                int pag = int.Parse(Console.ReadLine());
+                vect[i].SetPaginas(pag);
+            }
+
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("Veja os dados dos seus livros favoritos: ");
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(vect[i]);
+            }
+        }
+    }
+}
